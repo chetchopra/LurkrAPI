ca0687f6307f2fb6b818c00a5429c6c5213a4c81
chetchopra <chetanya.chopra94@gmail.com>
2019-07-26 10:06:46 -0700

    Logic to retrieve subreddit JSON has been moved to the subreddit model as opposed to the controller

--- Lurkr/app/controllers/subreddits_controller.rb
+++ Lurkr/app/controllers/subreddits_controller.rb
@@ -1,2 +1,14 @@
 class SubredditsController < ApplicationController
+
+  def index
+
+
+
+  end
+
+  def show 
+    subredditName = params[:id]
+    subredditPosts = Subreddit.getSubredditPosts(subredditName)
+    render json: subredditPosts  
+  end 
 end
--- Lurkr/app/models/subreddit.rb
+++ Lurkr/app/models/subreddit.rb
@@ -1,3 +1,32 @@
 class Subreddit < ApplicationRecord
   has_many :subscriptions
+
+  def self.hello 
+    puts("Hello")
+  end
+
+  def self.getSubredditPosts(subredditName)
+    # Hits provided subreddit url and parses the response
+    parsedResponse = JSON.parse(RestClient.get("http://reddit.com/r/#{subredditName}.json"))
+
+    # Traverses to the children (posts) in the response JSON
+    subredditPosts = parsedResponse["data"]["children"]
+
+    # TO-DO: Add in code to capture and handle errors 
+
+    posts = []
+
+    subredditPosts.each do |post|
+      posts << {title: post["data"]["title"],
+                img: post["data"]["thumbnail"],
+                url: post["data"]["url"],
+                num_comments: post["data"]["num_comments"],
+                author: post["data"]["author"],
+                self_text: post["data"]["selftext"]
+        }
+    end
+
+    return posts
+
+  end
 end
